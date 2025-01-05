a79ca6ff20f724519abd0192bf8c0f8454854aa4
Kye-Lee <kyelee.low@gmail.com>
2025-01-05 16:41:35 +0000

    added weather-app project

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,21 +19,33 @@ export default function About() {
                         />
                     </div>
                     <div className="flex justify-center items-center col-span-1 lg:col-span-2">
-                        <p className="whitespace-normal text-gray-900 dark:text-gray-100 text-xl">
-                            Hi, I’m Kye-Lee — a problem-solver, lifelong learner, 
-                            and now, an aspiring full-stack developer! 
-                            I’ve spent years creating environments where ideas thrive 
-                            and people succeed, and now I’m channeling that energy into 
-                            building intuitive, user-focused tech solutions. With a passion 
-                            for creativity, adaptability, and making things work, 
-                            I’m here to bring fresh ideas and a unique perspective to the 
-                            tech world. Explore my projects, see what I’ve been building!
-                        </p>
+                        <div>
+                            <p className="whitespace-normal text-gray-900 dark:text-gray-100 text-xl pb-5">
+                                Hi, I’m <b><i>Kye-Lee </i></b>!<br/> 
+                                I'm a <b> problem-solver, lifelong learner, 
+                                and an aspiring full-stack developer</b>.<br/> 
+                                After years of creating environments where ideas thrive 
+                                and people succeed, I’m now channeling that energy into 
+                                <b> building intuitive, user-focused tech solutions</b>. With a passion 
+                                for creativity, adaptability, and making things work, 
+                                I’m here to bring fresh ideas and a unique perspective to the 
+                                tech world. Explore my projects, see what I’ve been building!
+                            </p>
+                            <div className="py-12 text-2xl md:text-3xl lg:text-4xl flex space-x-10 justify-center">
+                                <i className="devicon-react-original-wordmark colored"></i>
+                                <i className="devicon-nextjs-original-wordmark light:colored"></i>
+                                <i className="devicon-javascript-plain colored"></i>
+                                <i className="devicon-typescript-plain colored"></i>
+                                <i className="devicon-html5-plain-wordmark colored"></i>
+                                <i className="devicon-css3-plain-wordmark colored"></i>
+                                <i className="devicon-bootstrap-plain-wordmark colored py-1"></i>
+                                <i className="devicon-tailwindcss-plain-wordmark colored text-6xl"></i>    
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="px-16 md:px-28">
-                <h3 className="text-2xl font-bold">Skills</h3>
+            {/* <div className="px-16 md:px-28">
                 <div className="py-5 text-xl md:text-2xl lg:text-4xl flex space-x-10 justify-center">
                     <i className="devicon-react-original-wordmark colored"></i>
                     <i className="devicon-nextjs-original-wordmark light:colored"></i>
@@ -44,7 +56,7 @@ export default function About() {
                     <i className="devicon-bootstrap-plain-wordmark colored py-1"></i>
                     <i className="devicon-tailwindcss-plain-wordmark colored text-6xl flex items-start"></i>    
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,29 +10,43 @@ export default function Projects() {
                 <hr className="h-1 my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
             </div>
             <div className="flex justify-center w-full absolute -z-50">
-                <div className="grid grid-cols-1 justify-items-center items-center lg:grid-cols-2 lg:gap-5">
-                    <div className="project-card-container relative width-1/2 lg:size-5/6 group flex justify-center">
+                <div className="grid grid-cols-1 justify-items-center items-center mb-20 lg:grid-cols-2 lg:gap-5">
+                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
                         <Image 
                             src="/recipe-app-home.png"
                             width={600}
                             height={600}
                             alt="Recipe App Homepage"
-                            className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-md"
+                            className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
                         />
                         <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
                             <a href="https://main.d3nfd799s6mstg.amplifyapp.com/" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Recipe App</a>
                         </div>
                     </div>
-                    <div className="project-card-container relative width-1/2 lg:size-5/6 group flex justify-center">
+                    
+                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
                         <Image 
-                            src="/grocery-list-app.png"
+                            src="/grocery-list-app.jpg"
                             width={600}
                             height={600}
                             alt="Grocery List App"
-                            className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-md"
+                            className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
                         />
                         <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
                             <a href="https://main.du85u0nlbn4bw.amplifyapp.com/" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Grocery List App</a>
+                        </div>
+                    </div>
+
+                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
+                        <Image 
+                            src="/weather-app.png"
+                            width={600}
+                            height={600}
+                            alt="Weather App"
+                            className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
+                        />
+                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
+                            <a href="https://main.d2c22tdmmbzffb.amplifyapp.com" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Weather App</a>
                         </div>
                     </div>
                 </div>
@@ -41,7 +55,6 @@ export default function Projects() {
             <style jsx>{`
                 .project-img {
                     transition: .5s ease;
-
                 }
 
                 .project-overlay {
