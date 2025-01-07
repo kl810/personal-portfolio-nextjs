bec41c0af24c8e339d3266d7585d18db8bec41e1
Kye-Lee <kyelee.low@gmail.com>
2025-01-07 19:16:18 +0000

    fixed button w-max

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,7 +11,7 @@ export default function Projects() {
             </div>
             <div className="flex justify-center w-full absolute -z-50">
                 <div className="grid grid-cols-1 justify-items-center items-center mb-20 lg:grid-cols-2 lg:gap-5">
-                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
+                    <div className="project-card-container relative w-3/4 lg:size-5/6 group flex justify-center">
                         <Image 
                             src="/recipe-app-home.png"
                             width={600}
@@ -19,12 +19,12 @@ export default function Projects() {
                             alt="Recipe App Homepage"
                             className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
                         />
-                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
+                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 w-max text-center group-hover:opacity-100">
                             <a href="https://food-waste-warrior.kyelee.dev" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Food Waste Warrior</a>
                         </div>
                     </div>
                     
-                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
+                    <div className="project-card-container relative w-3/4 lg:size-5/6 group flex justify-center">
                         <Image 
                             src="/grocery-list-app.jpg"
                             width={600}
@@ -32,12 +32,12 @@ export default function Projects() {
                             alt="Grocery List App"
                             className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
                         />
-                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
+                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 w-max text-center group-hover:opacity-100">
                             <a href="https://grocery-list.kyelee.dev" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Grocery List App</a>
                         </div>
                     </div>
 
-                    <div className="project-card-container relative w-1/2 lg:size-5/6 group flex justify-center">
+                    <div className="project-card-container relative w-3/4 lg:size-5/6 group flex justify-center">
                         <Image 
                             src="/weather-app.png"
                             width={600}
@@ -45,7 +45,7 @@ export default function Projects() {
                             alt="Weather App"
                             className="project-img m-2 h-auto w-100 block opacity-100 group-hover:opacity-30 rounded-2xl"
                         />
-                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 text-center group-hover:opacity-100">
+                        <div className="project-overlay opacity-0 absolute top-1/2 left-1/2 w-max text-center group-hover:opacity-100">
                             <a href="https://weather.kyelee.dev" className="overlay-text text-gray-200 bg-blue-500 dark:bg-indigo-500 hover:text-gray-100 py-4 px-8 rounded-full flex justify-center">Weather App</a>
                         </div>
                     </div>
